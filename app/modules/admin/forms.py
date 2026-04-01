13ce72b09a82a5a53181daf2b4b52f3715547824
IDGS-801-23001548 <84168@alumnos.utleon.edu.mx>
2026-03-31 21:34:35 -0600

    Limiar pycache

--- app/modules/admin/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
-
-class ComprasForm(FlaskForm):
-    proveedor = StringField('Proveedor', validators=[
-        DataRequired(message='El proveedor es obligatorio'),
-    ])
