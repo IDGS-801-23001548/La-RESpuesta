2335cbcd69b866c15111c894b23d5f87f955eea2
IDGS-801-23001533 <igames14444@gmail.com>
2026-04-02 16:44:27 -0600

    Se añadio carrito a los modelos 02/04/2026

---
+++ app/models/carrito.py
@@ -0,0 +1,16 @@
+from app.extensions import db
+from datetime import datetime
+
+class Carrito(db.Model):
+
+    __tablename__ = 'carrito'
+
+    idCarrito = db.Column(db.Integer, primary_key=True)
+    idUsuario = db.Column(db.Integer, db.ForeignKey('user.id'), unique=True)
+    fechaCreacion = db.Column(db.DateTime, default=datetime.utcnow)
+
+    productos = db.relationship(
+        'ProductoUnitario',
+        backref='carrito',
+        lazy='dynamic'
+    )
